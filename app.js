df69e8a34984a94c67de1bbbd0709609d631a50d
Mitchel <mitchelinajuo@gmail.com>
2019-09-03 22:14:29 +0100

    initial commit or whatever message for first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const path=require('path')
+const express =require('express')
+
+//
+const app =express();
+const port =process.env.port || "8080";
+
+//ejs is the view engine
+app.set('view engine','ejs')
+app.use('/static', express.static(path.join(__dirname, 'css')))
+app.use('/static', express.static(path.join(__dirname, 'images')))
+
+
+app.get('/', (req,res)=>{
+  res.render('pages/index')
+})
+
+app.get('/w3', (req,res)=>{
+  res.render('pages/w3')
+})
+
+app.get('/responsive', (req,res)=>{
+  res.render('pages/responsive')
+})
+
+app.get('/static', (req,res)=>{
+  res.send(console.log(path.join(__dirname, 'public')))
+})
+
+
+
+//about page
+app.get('/about', function(req,res){
+  res.render('pages/about')
+})
+
+app.listen(port,()=>{
+  console.log('listening to port 8080')
+})
